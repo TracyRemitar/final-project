928aa74d6dcf58e5358b7013747677f229f02e70
TracyRemitar <tracyrayremitar1399@gmail.com>
2023-12-12 23:30:36 +0800

    viewpage

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,12 +1,37 @@
 import React from 'react';
 
 const ViewPage = () => {
-    // Code to fetch and display inventory information
+    // Fetch inventory data from API or database
+    const inventoryData = [
+        { id: 1, name: 'Item 1', quantity: 10 },
+        { id: 2, name: 'Item 2', quantity: 5 },
+        { id: 3, name: 'Item 3', quantity: 8 },
+    ];
+
     return (
         <div>
-            {/* Display inventory information */}
+            <h1>Inventory View Page</h1>
+            <table>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Quantity</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {inventoryData.map((item) => (
+                        <tr key={item.id}>
+                            <td>{item.id}</td>
+                            <td>{item.name}</td>
+                            <td>{item.quantity}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </div>
     );
 };
 
 export default ViewPage;
+
