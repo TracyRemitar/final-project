07e3bcd715e995b725c9e1e8a091064f11ee921f
Robert Jay C. Quimno <rjcquimno@addu.edu.ph>
2023-12-13 16:34:40 +0800

    frontend delete

--- frontend/pages/delete.js
+++ frontend/pages/delete.js
@@ -1,19 +1,39 @@
-import React from 'react';
+// DeleteDataPage.js
+'use client';
+import React, { useState } from 'react';
 
 const DeleteDataPage = ({ item }) => {
-    const handleDelete = () => {
-        // Delete the data from the inventory
-        // Update the View Page with the updated data
-    };
+  const [isDeleting, setIsDeleting] = useState(false);
 
-    return (
-        <div>
-            <h1>Delete Data Page</h1>
-            <h2>{item.name}</h2>
-            <p>Quantity: {item.quantity}</p>
-            <button onClick={handleDelete}>Delete Data</button>
-        </div>
-    );
+  const handleDelete = async () => {
+    try {
+      // Set a loading state while the delete operation is in progress
+      setIsDeleting(true);
+
+      // Perform the delete operation (replace with your actual logic)
+      console.log('Deleting data...', item);
+
+      // Update the View Page with the updated data if necessary
+
+      // Reset the loading state when the operation is complete
+      setIsDeleting(false);
+    } catch (error) {
+      console.error('Error deleting data:', error);
+      // Handle error, display an error message, etc.
+      setIsDeleting(false);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Delete Data Page</h1>
+      <h2>{item.name}</h2>
+      <p>Quantity: {item.quantity}</p>
+      <button onClick={handleDelete} disabled={isDeleting}>
+        {isDeleting ? 'Deleting...' : 'Delete Data'}
+      </button>
+    </div>
+  );
 };
 
 export default DeleteDataPage;
--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,21 @@
-import ViewPage from '../../frontend/pages'
-import AddDataPage from '../../frontend/pages/add'
-import Link from 'next/link'
-import UpdateDataPage from '../../frontend/pages/update/[id]'
+// Home.js
+import React from 'react';
+import ViewPage from '../../frontend/pages';
+import AddDataPage from '../../frontend/pages/add';
+import UpdateDataPage from '../../frontend/pages/update/[id]';
+import DeleteDataPage from '../../frontend/pages/delete';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-      <ViewPage></ViewPage>
-      <AddDataPage></AddDataPage>
+        <ViewPage />
+        <AddDataPage />
       </div>
-      <div><UpdateDataPage></UpdateDataPage></div>
+      <div>
+        <UpdateDataPage />
+        <DeleteDataPage item={{ name: 'Example', quantity: 10 }} />
+      </div>
     </main>
-  )
+  );
 }
