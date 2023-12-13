0a2c8b6d5033664006e4804622d6cf6761ef6c17
TracyRemitar <tracyrayremitar1399@gmail.com>
2023-12-13 18:22:23 +0800

    inventory page

--- src/app/page.js
+++ src/app/page.js
@@ -3,15 +3,13 @@ import ViewPage from '../../frontend/pages';
 import AddDataPage from '../../frontend/pages/add';
 import UpdateDataPage from '../../frontend/pages/update/[id]';
 import DeleteDataPage from '../../frontend/pages/delete';
+import Inventory from '../../frontend/pages/Inventory';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <ViewPage />
-        <AddDataPage />
-        <UpdateDataPage />
-        <DeleteDataPage item={{ name: 'Example', quantity: 10 }} />
+      <div>
+        <Inventory></Inventory>
       </div>
     </main>
   );
